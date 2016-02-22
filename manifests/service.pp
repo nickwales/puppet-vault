d5ffd3d731fe9788db9967459053338df23269cf
Nick Wales <nwales@homeaway.com>
2016-02-22 17:18:59 -0600

    errant $

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,6 @@
 # == Class vault::service
 class vault::service (
-  $service_prodvider = $::vault::params::$service_prodiver  
+  $service_prodvider = $::vault::params::service_provider  
 ){
   service { $::vault::service_name:
     ensure   => running,
